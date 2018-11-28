a7b8729e06a446bf62d8f92264a2f13dc8cc1ceb
Magusdark <43154087+Magusdark@users.noreply.github.com>
2018-11-27 22:07:14 -0200

    Update troca.c

--- Exercicio 5.4/troca.c
+++ Exercicio 5.4/troca.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 void troca(int* p1, int* p2){
-        int* p3; int z;
-        p3=&z;
-        *p3=*p2;
+        int z;
+        z=*p2;
         *p2=*p1;
-        *p1=*p3;
+        *p1=z;
 }
 int main(){
-    int x , y ; int* p1;
-    int* p2;
+    int x , y ;
     printf("Escolha um valor para x e para y , respectivamente: \n");
     scanf("%d %d", &x , &y);
-    p1=&x;
-    p2=&y;
-    troca(p1,p2);
+    troca(&x,&y);
     printf("Os valores foram trocados:\nX=%d\nY=%d", x , y);
     return 0;
     }
