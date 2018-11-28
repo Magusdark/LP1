1a57bed35e4d3ca33b36fc185fd8de3ad61990c0
Magusdark <43154087+Magusdark@users.noreply.github.com>
2018-11-27 22:04:09 -0200

    Update compras.c

--- Exercicio 5.3/compras.c
+++ Exercicio 5.3/compras.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-void compra(int* conta, int valor,int outra_conta){
+void compra(int* conta, int valor){
 	*conta-=valor;
-	printf("Compra Efetuada na conta de maior saldo.\n Saldo atual:%d\nOutra conta: %d\n", *conta, outra_conta);
 }
 
 int main (void){
 	int minha_conta1 , minha_conta2 , compras[]={100, 50, 80, 30, 20}, valor , outra_conta, i; int* conta;
 	printf("Insira o saldo da sua conta: \n");
 	scanf("%d %d" , &minha_conta1 , &minha_conta2);
-	if (minha_conta1<minha_conta2){
-		conta=&minha_conta2;
-		outra_conta=minha_conta1;
-	}else{
-		conta=&minha_conta1;
-		outra_conta=minha_conta2;
-	}
 	for (i=0;i<5;i++){
+        if (minha_conta1<minha_conta2){
+            conta=&minha_conta2;
+            outra_conta=minha_conta1;
+        }else{
+            conta=&minha_conta1;
+            outra_conta=minha_conta2;
+        }
         valor=compras[i];
-        compra(conta, valor, outra_conta);
+        compra(conta, valor);
+        printf("Compra Efetuada na conta de maior saldo.\n Saldo atual:%d\nOutra conta: %d\n", *conta, outra_conta);
 	}
-	printf("Os saldos das duas contas são:%d \n%d \n", *conta , outra_conta);
 	return 0;
 }
+
