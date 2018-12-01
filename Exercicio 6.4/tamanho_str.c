7450ae7ad58a71a637e5967c92fd6874ab07d513
Magusdark <43154087+Magusdark@users.noreply.github.com>
2018-12-01 17:24:43 -0200

    Update tamanho_str.c

--- Exercicio 6.4/tamanho_str.c
+++ Exercicio 6.4/tamanho_str.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int strlen2(char* str){
-	int len;
-	len = strlen(str);
-	return len;
+	int len=0;
+	char x=str[len];
+	while( x!='\0'){
+        x=str[len];
+        len+=1;
+	}
+    return (len-1);
 }
+
 
 int main(void){
 	char nam[30];
 	scanf("%s", nam);
 	int x = strlen2(nam);
-	printf("O tamanho da str %s é %d\n", nam, x);
-	return 0;
+	printf("O tamanho da str %s eh: %d\n", nam, x);
+    return 0;
 }
 
