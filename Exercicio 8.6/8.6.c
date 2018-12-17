68fe402125a675171d4c1fb669d5b3f795444556
Magusdark <43154087+Magusdark@users.noreply.github.com>
2018-12-17 09:50:45 -0200

    Update 8.6.c

--- Exercicio 8.6/8.6.c
+++ Exercicio 8.6/8.6.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -12,14 +13,13 @@ int main(void){
     int n;
     printf("Digite o numero de pessoas:\n");
     scanf("%d", &n);
-    FILE* f= fopen("C:/Users/Cliente/Documents/C/Arquivos/ex8.bin", "wb");
+    FILE* f= fopen("/home/ime/ex8.bin", "wb");
     fwrite(&n, sizeof(int), 1, f);
     for (int i=0;i<n;i++){
         printf("Insira o a idade e o nome da pessoa, respectivamente\n");
         scanf("%d", &v[i].idade);
         scanf("%s", v[i].nome);
-        fwrite(&v[i].idade, sizeof(int), 1, f);
-        fwrite(v[i].nome, sizeof(char), (strlen(v[i].nome))+1, f);
+        fwrite(&v[i], sizeof(struct Pessoa), 1,f);
     }
     fclose(f);
     return 0;
